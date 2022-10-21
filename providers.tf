f49a0145266df8c54a196e2a77fa220092829a89
Theuns Steyn <theuns.steyn@csgi.com>
2022-10-21 14:38:31 +0000

    Syntax correction with variables

--- providers.tf
+++ providers.tf
@@ -6,7 +6,7 @@ terraform {
     }
     docker = {
       source  = "kreuzwerker/docker"
-      version = "~> 2.0.0"
+      version = "~> 2.20.0"
     }
   }
 }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,14 @@
 variable "vpc_id" {
-  value = "vpc-00c4c78c22e1922a9"
+  description = "VPC ID to use for AWS resources"
+  default = "vpc-00c4c78c22e1922a9"
 }
 
 variable "subnet1_id" {
-  value = "subnet-0b21f234b0f4fc6d4"
+  description = "The first subnet to use"
+  default = "subnet-0b21f234b0f4fc6d4"
 }
 
 variable "subnet2_id" {
-  value = "subnet-0bf1f7a7538566a79"
+  description = "The second subnet to use"
+  default = "subnet-0bf1f7a7538566a79"
 }
