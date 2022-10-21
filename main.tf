80d68b6e7420ec6e29e1c1e6facdfbe2d7fd9e58
Theuns Steyn <theuns.steyn@csgi.com>
2022-10-21 02:56:55 +0000

    getting some basics done

--- main.tf
+++ main.tf
@@ -1,1 +1,16 @@
 # This is the main terraform file for doing stuff
+
+resource "aws_security_group" "allow_http" {
+  name        = "allow_http"
+  description = "Allow HTTP inbound traffic"
+  vpc_id      = "vpc-00c4c78c22e1922a9"
+
+ingress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+}
