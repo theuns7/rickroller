5d76d40f3c1e48765d1088ed962a8ee860f4f2b0
Theuns Steyn <theuns.steyn@csgi.com>
2022-10-22 02:43:47 +0000

    Adding some egress ports to sg

--- main.tf
+++ main.tf
@@ -68,4 +68,22 @@ resource "aws_security_group" "allow_http" {
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
+  egress {
+    description      = "HTTP"
+    from_port        = 80
+    to_port          = 80
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    description      = "HTTPS"
+    from_port        = 443
+    to_port          = 443
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
 }
